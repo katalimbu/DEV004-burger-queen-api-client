6f76296ad57571c0b87b852093d21ce6110d8f83
LuzAlvaca <luz.alvaca@gmail.com>
2023-05-30 10:30:02 -0600

    router

--- burger-queen-react/src/main.jsx
+++ burger-queen-react/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+// import App from './App.jsx'
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import "./index.css"
+
+const router = createBrowserRouter([
+  {
+    path: "/kitchen",
+    element: <div> <h1>hellow</h1></div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
-)
+);
+
+
+
+
