2b826b3281be857b789e6617277172db0fd02b77
katalimbu <katalina.esteban@gmail.com>
2023-05-30 23:23:18 -0400

    router avanzado a breakfast

--- burger-queen-react/src/main.jsx
+++ burger-queen-react/src/main.jsx
@@ -4,12 +4,17 @@ import ReactDOM from "react-dom/client"
 import {createBrowserRouter,RouterProvider} from "react-router-dom"
 import "./index.css"
 import Login from "./components/Login/Login.jsx"
+import Breakfast from "./components/Products/Breakfast"
 
 const router = createBrowserRouter([
   {
     path: "/login",
     element: <Login />
   },
+  {
+    path: "/breakfast",
+    element: <Breakfast />
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
