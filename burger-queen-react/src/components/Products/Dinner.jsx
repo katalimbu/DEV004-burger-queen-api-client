ab98bd5e082625691466843e962db55a326c4a1f
LuzAlvaca <luz.alvaca@gmail.com>
2023-05-31 11:40:27 -0600

    estructura vista Dinner sin estilos, fuente universal cargada

--- burger-queen-react/src/components/Products/Dinner.jsx
+++ burger-queen-react/src/components/Products/Dinner.jsx
@@ -1,4 +1,31 @@
+import "./dinner.css"
+
 function Dinner (){
-    return <h>Hola mesero </h>
+    return (
+        <div>
+        
+        <div className="containerDinner"> 
+        <h1>Hola mesero </h1>
+        <label>
+        Nombre del cliente: <input name="clientName" />
+      </label>
+        <div className ="containerItemsDinner">
+            <h2> Resumen del pedido:</h2>
+            <btn>Hamburguesa Simple $10 </btn>
+            <btn>Hamburguesa Doble $15 </btn>
+            <btn>Papas fritas $5 </btn>
+            <btn>Aros de cebolla $5 </btn>
+            <btn>Agua chica $5  </btn>
+            <btn>Agua grande $7 </btn>
+            <btn>Gaseosa chica $5 </btn>
+            <btn>Gaseosa grande $7 </btn>
+        </div>
+        <div className="orderDinner">
+        </div>
+        <btn className= "OrderReadyForKitchen">Enviar a cocina</btn>
+        </div>
+        </div>
+    )
+    
 }
 export default Dinner
