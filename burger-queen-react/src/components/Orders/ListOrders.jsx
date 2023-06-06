e59976a36034ae1a09becc750c4c931839d76d8b
katalimbu <katalina.esteban@gmail.com>
2023-06-05 23:23:12 -0400

    order inconcluso, estoy tratando de almacenar en algun lado la informacion de los pedidos

--- burger-queen-react/src/components/Orders/ListOrders.jsx
+++ burger-queen-react/src/components/Orders/ListOrders.jsx
@@ -1,15 +1,13 @@
-import  { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import logo from '../../assets/logo.png';
 
-function ListOrders(){ 
+function ListOrders() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [orderStatus, setOrderStatus] = useState('orders');
-
-
-  useEffect(()=>{
+  
+  useEffect(() => {
     const accessToken = localStorage.getItem('token');
 
     axios.get('http://localhost:8080/orders', {
@@ -26,8 +24,9 @@ function ListOrders(){
         setError('Error al obtener los datos');
         setIsLoading(false);
       });
-      // los [] son una lista de dependencias, osea que cada ves que algo dentro de la lista cambie, se ejecuta el useEffect
-  },[]);
+  }, []);
+
+
   if (isLoading) {
     return <div>Cargando...</div>;
   }
@@ -35,33 +34,131 @@ function ListOrders(){
   if (error) {
     return <div>{error}</div>;
   }
-  console.log(data)
 
-  const filterOrder = data.filter(order => order.status == "pending");
+  const handleButtonClick = (orderId) => {
+    const accessToken = localStorage.getItem('token');
 
+    axios.patch(`http://localhost:8080/orders/${orderId}`, {
+      status: 'delivered',
+    }, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        Authorization: `Bearer ${accessToken}`
+      }
+    }
+  )
+  .then(response => {
+    // Mensaje de éxito al entregar el pedido
+    alert('El pedido ha sido entregado con éxito');
+        // setData(response.data);
+        // setIsLoading(false);
+        // console.log(response);
+      })
+      .catch(error => {
+        console.error(error);
+        setError('Error al entregar el pedido');
+        setIsLoading(false);
+        // Mensaje de error al entregar el pedido
+        alert('Hubo un error al entregar el pedido. Por favor, inténtalo nuevamente.');
+      });
+  };
 
-  return(
+  const filterOrder = data.filter(order => order.status === "pending");
+
+  return (
     <div>
       <img src={logo} alt="Logo" />
       <h1>Estado del Pedido</h1>
       <div>
-      {filterOrder.map(order => (
-        <div key={order.id}>
-          <h3>{order.id}</h3>
-          {order.products.map(item => (
-            <div key={item.product.id}>
-              <p>cantidad: {item.qty}</p>
-              <p>nombre: {item.product.name}</p>
-            </div>
-          ))}
-                <button onClick={()=>setOrderStatus('orders')}>Listo</button>
-
-        </div>
-      ))}
+        {filterOrder.map(order => (
+          <div key={order.id}>
+            <h3>{order.id}</h3>
+            {order.products.map(item => (
+              <div key={item.product.id}>
+                <p>cantidad: {item.qty}</p>
+                <p>nombre: {item.product.name}</p>
+              </div>
+            ))}
+            <button onClick={() => handleButtonClick(order.id)}>Listo</button>
+          </div>
+        ))}
       </div>
     </div>
-  )
-
+  );
 }
 
 export default ListOrders;
+
+// import  { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import logo from '../../assets/logo.png';
+
+// function ListOrders(){ 
+//   const [data, setData] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState(null);
+//   const [orders, setOrders] = useState(filterOrder);
+
+
+
+//   useEffect(()=>{
+
+//       const filterOrder = data.filter(order => order.status === "pending");
+//       setOrders(filterOrder);
+//     },[data]);
+//     const accessToken = localStorage.getItem('token');
+
+//     axios.get('http://localhost:8080/orders', {
+//       headers: {
+//         Authorization: `Bearer ${accessToken}`
+//       }
+//     })
+//       .then(response => {
+//         setData(response.data);
+//         setIsLoading(false);
+//       })
+//       .catch(error => {
+//         console.error(error);
+//         setError('Error al obtener los datos');
+//         setIsLoading(false);
+//       });
+//       // los [] son una lista de dependencias, osea que cada ves que algo dentro de la lista cambie, se ejecuta el useEffect
+ 
+//   if (isLoading) {
+//     return <div>Cargando...</div>;
+//   }
+
+//   if (error) {
+//     return <div>{error}</div>;
+//   }
+//   console.log(data)
+
+//   const handleButtonClick = () => {
+//     const updatedOrders = orders.filter(order => order.status !== "pending");
+//     setOrders(updatedOrders);
+//   };
+
+//   return(
+//     <div>
+//       <img src={logo} alt="Logo" />
+//       <h1>Estado del Pedido</h1>
+//       <div>
+//       {orders.map(order => (
+//         <div key={order.id}>
+//           <h3>{order.id}</h3>
+//           {order.products.map(item => (
+//             <div key={item.product.id}>
+//               <p>cantidad: {item.qty}</p>
+//               <p>nombre: {item.product.name}</p>
+//             </div>
+//           ))}
+//          <button onClick={handleButtonClick}>Listo</button>
+//         </div>
+//       ))}
+//       </div>
+//     </div>
+//   )
+
+// }
+
+// export default ListOrders;
